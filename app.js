b9ffbb1c0ede5a837ca1e4fe93e5cb17565c3ef2
Nawanit <sharma.nawanit123@gmail.com>
2020-03-18 17:06:46 +0530

    Create DATABASEURL ENVIRONMENT VARIABLE

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
-// mongoose.connect("mongodb://localhost:27017/yelp_camp");
-mongoose.connect("mongodb+srv://Nawanit:password101@cluster0-wrjc7.mongodb.net/test");
+
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb+srv://Nawanit:password101@cluster0-wrjc7.mongodb.net/test");
 //
 app.use(
   require("express-session")({
