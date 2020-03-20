00f830cb96402a5bc9a31d70f2b19eec1a1cda33
Nawanit <sharma.nawanit123@gmail.com>
2020-03-20 16:22:05 +0530

    Edited seedDB with better promise writing

--- seedDb.js
+++ seedDb.js
@@ -16,6 +16,57 @@ const seeds=[
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem in cum alias perferendis rerum! Mollitia voluptatum ut sunt soluta ab dolores assumenda laboriosam, quis optio maiores nulla reprehenderit quam velit quidem et molestias, illum eius autem magni suscipit aut modi, ipsum veniam a. Eveniet nemo magni sint ad! Magni, quaerat?" 
     }
 ];
+
+// function seedDB(){
+//     const myPromise1=()=>{
+//         const promise1=new Promise((resolve,reject)=>{
+//             resolve(Campground.deleteMany({}));
+//         });
+//         return promise1;
+//     };
+
+//     const myPromise2=(seed)=>{
+//         const promise2=new Promise((resolve,reject)=>{
+//             resolve(Campground.create(seed));
+//         });
+//         return promise2;
+//     }
+//     const myPromise3=()=>{
+//         const promise3 = new Promise((resolve,reject)=>{
+//             resolve(Comment.create({
+//                 text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae officia fuga quasi inventore placeat similique cupiditate illo, omnis provident repudiandae, minus dignissimos quidem ullam quas cum qui accusantium praesentium aliquam officiis sint dolores? Doloribus et dicta fuga. Ipsum delectus error quaerat maiores iste nemo officia perspiciatis dolores soluta maxime?",
+//                 author: "Homer"
+//             }));
+//         });
+//         return promise3;
+//     }
+
+
+// myPromise1().then(text=>{
+//     console.log("Campground Removed");
+//     return new Promise((resolve,reject)=>{
+//         resolve(Comment.deleteMany({}));
+//     });
+// }).then(text=>{
+//     console.log("Comments Removed");
+//     for (const seed of seeds){
+//         myPromise2(seed)
+//         .then((campground)=>{
+//             console.log("Campground Created!!");
+//             myPromise3().then((comment)=>{
+//                 console.log("Comment Created");
+//                 campground.comments.push(comment);
+//                 campground.save();
+//                 console.log("Comment added to Campgrounds");
+//             }).catch(err=>{
+//                 console.log(err);
+//             })
+//         }).catch(err=>{
+//             console.log(err);
+//             })
+//     }
+// });
+// }
 async function seedDB(){
     try{
         //remove Campgrounds
@@ -43,63 +94,5 @@ async function seedDB(){
         console.log(err);
     }
 }
-
-// function seedDB(){
-//    const myPromise1=()=>{
-//        return new Promise((resolve,reject)=>{
-//          resolve(Campground.deleteMany({}));
-//        })
-//    }
-//    const myPromise2=()=>{
-//        return new Promise((resolve,reject)=>{
-           
-//            resolve(Comment.deleteMany({}));
-//        })
-//    }
-//     const myPromise3 =(seed)=>{
-//            return new Promise((resolve,reject)=>{
-//                    resolve(Campground.create(seed));
-//            })
-//     }
-//     const myPromise4=()=>{
-//         return new Promise((resolve,reject)=>{
-//                 resolve(Comment.create(
-//                     {
-//                         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae officia fuga quasi inventore placeat similique cupiditate illo, omnis provident repudiandae, minus dignissimos quidem ullam quas cum qui accusantium praesentium aliquam officiis sint dolores? Doloribus et dicta fuga. Ipsum delectus error quaerat maiores iste nemo officia perspiciatis dolores soluta maxime?",
-//                         author: "Homer"
-//                     }
-    
-//                 ));
-//         })
-//     }
-
-//     myPromise1()
-//     .then(()=>{
-//             console.log("Campground Removed");
-//     })
-//     .then(myPromise2)
-//     .then(()=>{
-//         console.log("Comment Removed")
-//     })
-//     .then(()=>{
-//         for(const seed of seeds){
-//             myPromise3(seed).then((campground)=>{
-//                 console.log("Campground Created");
-//                 myPromise4().then((comment)=>{
-//                     console.log("Comment Created");
-//                     campground.comments.push(comment);
-//                     campground.save();
-//                     console.log("Comment added to Campground");
-//                 }).catch(err=>{
-//                     console.log(err);
-//                 })
-//             }).catch(err=>{
-//                 console.log(err);
-//             })
-//         }
-//     }).catch(err=>{
-//         console.log(err);
-//     })
-// }
 module.exports=seedDB;
 
